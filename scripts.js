9c1c3ecb3365a79ae5317f25564649b1a4c07c58
w3ssfs <wesleyfranca001@gmail.com>
2022-07-18 20:15:03 -0300

    Modificações no css e criação do JS

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function img() {
+    document.getElementById("images").src = "image/sakura.webp";
+    document.getElementById("name").innerHTML = "Sakura Haruno";
+    document.getElementById("des").innerHTML = "Aprendiz da tsunadi";
+
+}
+
+function img1() {
+    document.getElementById("images").src = "image/sasuke.jpg";
+    document.getElementById("name").innerHTML = "Sasuke Uchiha";
+    document.getElementById("des").innerHTML = "Maldição do ódio";
+}
+
+function img2() {
+    document.getElementById("images").src = "image/naruto.png";
+    document.getElementById("name").innerHTML = "Naruto Uzumaki";
+    document.getElementById("des").innerHTML = "Naruto dos 9 rabo";
+}
